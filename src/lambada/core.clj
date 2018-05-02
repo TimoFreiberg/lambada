956e6426ce8e64d23097cabe30668164d18cd21f
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-05-02 13:51:39 +0200

    Remove spec, validate parameters manually
    To remove clojure 1.9 dependency

--- src/lambada/core.clj
+++ src/lambada/core.clj
@@ -1,11 +1,14 @@
 (ns lambada.core
-  (:require [lambada.spec :as spec])
   (:import [com.amazonaws.services.lambda.runtime RequestStreamHandler]))
 
 (defmacro deflambdafn
   "Create a named class that can be invoked as a AWS Lambda function."
   [name args & body]
-  (spec/assert-args [name args body])
+  (assert (symbol? name) "Lambda handler class name must be a symbol")
+  (assert (and (vector? args)
+               (or (= 2 (count args))
+                   (= 3 (count args))))
+          "Lambda handler args must be a vector of 2 or 3 elements")
   (let [prefix (gensym)
         handleRequestMethod (symbol (str prefix "handleRequest"))]
     `(do
